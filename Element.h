d08a470b0a1ccd661867cd1232fa6c9c182c245d
Cjescalona1 <cjescalona1@gmail.com>
2015-04-02 17:14:20 -0430

    Create Element.h
    verificar comentario en los constructores.

---
+++ Element.h
@@ -0,0 +1,40 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class Element
+{
+	private:
+		string Tag, Inner;
+		list<string> Listattr;
+	public:
+		///constructores
+		Element():Tag(""), Inner(""), Listattr(){};
+		Element(string tag): Tag(tag), Inner(""), Listattr(){};
+		Element(string tag, const list<string> &l1): Tag(tag), Listattr(l1){/**cout<<endl<<"const string and list "<<endl;*/} 
+	  Element(string tag, string inner): Tag(tag), Inner(inner), Listattr(){/**cout<<"const doble string"<<endl;*/}
+		Element(string tag, string inner, const list<string> &l1): Tag(tag), Inner(inner), Listattr(l1){};
+
+		///consultores
+		string tagName() const{return(Tag);}
+		list<string> attributeList() const{return(Listattr);}
+		string innerHTML() const {return(Inner);}
+
+		///modificadores
+		void setTagName(const string &tag){this->Tag=tag;}
+		void setAttributeList(const list<string> &l1){this->Listattr= l1;}
+		void setInnerHTML(const string &inner){this->Inner= inner;}
+
+		///sobreCargas
+		bool operator==(const Element& e2) const{return((Tag==e2.Tag) && (Inner==e2.Inner)&& (Listattr==e2.Listattr));}
+		bool operator!=(const Element& e2) const{return(!((*this)==e2));}
+		void operator=(const Element& e2){Tag=e2.Tag;Inner=e2.Inner;Listattr=e2.Listattr;}
+		friend istream& operator>> (istream& is, Element& ex){is>> ex.Tag;return is;}
+		friend ostream& operator<< (ostream& os, const Element& ex){os<<ex.Tag;return os;}
+};
+#endif
