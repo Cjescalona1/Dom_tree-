74bfc022c28cf8d5f4111fcfba470b0053f49919
iluigi77 <luisjosegonzaleztovar@gmail.com>
2015-04-10 20:18:24 -0430

    Update DOM_Tree.h
    constructor copia, sobre carga del flujo de entrada, constructor a partir de un string, metodos appendChild y replaceChild con string, eliminacion de atributo que no se usa

--- DOM_Tree.h
+++ DOM_Tree.h
@@ -12,7 +12,7 @@ using namespace std;
 class DOM_Tree
 {
 	private:
-		Node *nodeRaiz, *Seed;
+		Node *nodeRaiz;
 		
 		///estaticos
 		static Node* copiarNode(Node *ptr);
@@ -20,9 +20,10 @@ class DOM_Tree
 					
 	public:
 		///constructores
-		DOM_Tree(): nodeRaiz(NULL), Seed(NULL){}
-		DOM_Tree(const Element &e1){nodeRaiz= new Node(e1); Seed=NULL}
+		DOM_Tree(): nodeRaiz(NULL){}
+		DOM_Tree(const Element &e1){nodeRaiz= new Node(e1);}
 		DOM_Tree(const Element &raiz, list<DOM_Tree> &l1);
+		DOM_Tree (const DOM_Tree &dt){this->copiar(dt);}
 		///consultores
 		Element getRaizDT()const{return(nodeRaiz->element());}
 		DOM_Tree childNode(int p)const;
@@ -32,10 +33,17 @@ class DOM_Tree
 		void removeChild(int p);
 		void appendChild(int p, const DOM_Tree &nvo);
 		void appendChild(const DOM_Tree &nvo);
+		void appendChild(int p, string cadena);
+		void appendChild(string cadena);
 		void replaceChild(int p, const DOM_Tree &nvo);
+		void replaceChild(int p, string cadena);
+		void buildString(string &cadena);
 		
 		///sobreCargas
 		void operator=(const DOM_Tree &a2){this->copiar(a2);}
+		friend istream& operator>> (istream& is, DOM_Tree &ax);
+
+		
 		///Destructores
 	
 };		
@@ -82,7 +90,7 @@ class DOM_Tree
 	
 	Node* DOM_Tree:: buscarByID(string ID, Node *ptr)
 	{
-	Node *sol=NULL;	
+		Node *sol=NULL;	
 		if (ptr!=NULL)
 		{
 			if (!(ptr->element().attributeList().empty()))//si la lista de atributos del elemento NO ES VACIA
@@ -154,9 +162,8 @@ class DOM_Tree
 			nodeRaiz= new Node(aDT.nodeRaiz->element(),copiarNode(aDT.nodeRaiz->firstChild()), NULL);
 		else
 			nodeRaiz=NULL;
+		ident= aDT.ident;
 	}
-	
-	
 	
 	void DOM_Tree:: removeChild(int p)
 	{
@@ -193,6 +200,7 @@ class DOM_Tree
 			ab.nodeRaiz=NULL;
 		}
 	}
+	
 	void DOM_Tree:: appendChild(int p, const DOM_Tree &nvo)
 	{
 		int i=1;
@@ -230,8 +238,6 @@ class DOM_Tree
 		}
 	}
 
-
-
 	void DOM_Tree:: appendChild(const DOM_Tree &nvo)
 	{
 		Node *aux;
@@ -253,8 +259,24 @@ class DOM_Tree
 		}
 	}
 	
-	
-	
+	void DOM_Tree:: appendChild(int p, string cadena)
+	{
+		DOM_Tree nvo;
+
+		nvo.buildString(cadena);
+		this->appendChild(p, nvo);
+		nvo.nodeRaiz=NULL;
+	}
+
+	void DOM_Tree:: appendChild(string cadena)
+	{
+		DOM_Tree nvo;
+
+		nvo.buildString(cadena);
+		this->appendChild(nvo);
+		nvo.nodeRaiz=NULL;
+	}
+
 	void DOM_Tree:: replaceChild(int p, const DOM_Tree &nvo)
 	{
 		int i=1;
@@ -299,3 +321,192 @@ class DOM_Tree
 		}
 	}
 	
+	void DOM_Tree:: replaceChild(int p, string cadena)
+	{
+		DOM_Tree nvo;
+
+		nvo.buildString(cadena);
+		this->replaceChild(p, nvo);
+		nvo.nodeRaiz=NULL;
+	}
+
+	void DOM_Tree:: buildString(string &cadena)
+	{
+		string s1;
+		list<string> l;
+		Element e("",l);
+		bool band;
+		DOM_Tree hermanos, hijos;
+		unsigned n;
+		
+		this->vaciarDT();
+
+		if(!cadena.empty() )
+		{
+			while(cadena[0]!='<' && !cadena.empty())
+				cadena.erase(0,1);
+			if(!cadena.empty())
+			{
+				cadena.erase(0,1);
+				n=cadena.find('>');
+				if(n!=string::npos)
+				{
+					s1=cadena.substr(0, n);
+					cadena.erase(0,n+1);
+				}
+				if ((s1=="!doctype html") || (s1=="!DOCTYPE html"))
+				{
+					e.setTagName("!doctype html");
+					this->nodeRaiz=new Node(e);/***/
+					hijos.buildString(cadena);
+					this->nodeRaiz->setFirstChild(hijos.nodeRaiz);/**/
+					hijos.nodeRaiz=NULL;
+				}
+				else if (s1[0]!='/')
+				{
+					while(s1[0]==' ')
+						s1.erase(0,1);
+					n= s1.find(" ");
+					if(n==string::npos)
+						e.setTagName(s1);
+					else
+					{
+						e.setTagName(s1.substr(0,n));
+						s1.erase(0, n+1);
+						while(s1[0]==' ')
+							s1.erase(0,1);
+							
+						n= s1.find(" ");
+						while (n!=string::npos )
+						{
+							l.push_back(s1.substr(0,n));
+							s1.erase(0, n+1);
+							n= s1.find(" ");
+						}
+						if(s1.length()>0)
+						{
+							l.push_back(s1);
+							s1.erase(0);
+						}
+						e.setAttributeList(l);
+					}
+					band=0;
+
+					n=cadena.find('<');
+					if(n!= string::npos)
+					{
+						s1=cadena.substr(0, n);
+						cadena.erase(0,n);
+					}
+					n=s1.find('\n');
+					if(n!= string::npos)
+						s1.erase(n);
+					if(s1.length()>0)
+						e.setInnerHTML(s1);
+					n=cadena.find('>');
+					if(n!= string::npos)
+						s1=cadena.substr(0, n+1);
+					if ((s1=="</"+e.tagName()+">"))//si encuentra el cierre de la cabecera
+					{
+						band=1;
+						cadena.erase(0,n+1);
+					}
+					this->nodeRaiz= new Node(e);
+					if (!band)
+					{
+						hijos.buildString(cadena);
+						this->nodeRaiz->setFirstChild(hijos.nodeRaiz);
+						hijos.nodeRaiz=NULL;
+					}
+					hermanos.buildString(cadena);
+					this->nodeRaiz->setNextSibling(hermanos.nodeRaiz);
+					hermanos.nodeRaiz=NULL;
+				}
+			}
+		}
+
+
+
+	}
+	
+	///sobreCargas
+	istream& operator>> (istream& is, DOM_Tree &ax)
+	{	
+		string s1;
+		list<string> l;
+		Element e("",l);
+		bool band;
+		DOM_Tree hermanos, hijos;
+		unsigned n;
+		
+		ax.vaciarDT();
+		if(getline(is, s1, '<'))
+		{
+			if (getline(is, s1, '>'))
+			{
+				if (s1=="!doctype html")
+				{
+					e.setTagName("!doctype html");
+					///ax.Seed=new Node(e);
+					ax.nodeRaiz=new Node(e);/***/
+					is>>hijos;
+					///ax.Seed->setFirstChild(hijos.nodeRaiz);
+					ax.nodeRaiz->setFirstChild(hijos.nodeRaiz);/**/
+					///ax.nodeRaiz=hijos.nodeRaiz;
+					hijos.nodeRaiz=NULL;
+				}
+				else if (s1[0]!='/')
+				{
+					while(s1[0]==' ')
+						s1.erase(0,1);
+					n= s1.find(" ");
+					if(n==string::npos)
+						e.setTagName(s1);
+					else
+					{
+						e.setTagName(s1.substr(0,n));
+						s1.erase(0, n+1);
+						while(s1[0]==' ')
+							s1.erase(0,1);
+						n= s1.find(" ");
+						while (n!=string::npos )
+						{
+							l.push_back(s1.substr(0,n));
+							s1.erase(0, n+1);
+							n= s1.find(" ");
+						}
+						if(s1.length()>0)
+						{
+							l.push_back(s1);
+							s1.erase(0);
+						}
+						e.setAttributeList(l);
+					}
+					band=0;
+					getline(is, s1);//leer el inner
+					n=s1.find("</"+e.tagName()+">");
+					if ((n!=string::npos))//si encuentra el cierre de la cabecera despues de la ṕrimera posicion, indica un innerHTML
+					{
+						band=1;
+						s1= s1.substr(0, (s1.length()-(e.tagName().length()+3)));
+					}
+					e.setInnerHTML(s1);
+					ax.nodeRaiz= new Node(e);
+					if (!band)
+					{
+						is>>hijos;
+						ax.nodeRaiz->setFirstChild(hijos.nodeRaiz);
+						hijos.nodeRaiz=NULL;
+					}
+					is>>hermanos;
+					ax.nodeRaiz->setNextSibling(hermanos.nodeRaiz);
+					hermanos.nodeRaiz=NULL;
+				}
+			}
+		}
+		return is;
+	}
+		
+		
+		
+#endif
